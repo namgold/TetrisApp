b28a029cad7f51fec4c752c31eab0642c13cf6f7
Nam Nguyen <hnamnguyencse@gmail.com>
2017-11-08 13:29:04 +0700

    second commit

---
+++ bricks.py
@@ -0,0 +1,59 @@
+#==============================================================================================#
+# Name        : bricks.py                                                                      #
+# Description : The brick class definition for the tetris game.                                #
+# Author      : Nguyen Hoang Nam                                                               #
+# Date        : 07.11.2017                                                                     #
+#==============================================================================================#
+
+
+from random import randrange
+from colors import *
+class brick:
+	position = [0,0]
+	type = 0
+	bricksShape = [
+	[[]],
+
+	[[0,1,0],
+	 [1,1,1]],
+
+	[[1,1,0],
+	 [0,1,1]],
+
+	[[0,1,1],
+	 [1,1,0]],
+
+	[[1,1],
+	 [1,1]],
+
+	[[1,1,1,1],
+	 [0,0,0,0]],
+
+	[[1,0,0],
+	 [1,1,1]],
+
+	[[0,0,1],
+	 [1,1,1]]
+	]
+	shape=[[]]
+	def __init__(self,Width):
+		self.type = randrange(1,8)
+		self.shape=self.bricksShape[self.type]
+		self.position=[0,Width//2-1]
+
+	def rotate(self):
+		self.shape=list(zip(*self.shape[::-1]))
+
+	def fall(self):
+		self.position[0]+=1
+
+	def moveLeft(self):
+		self.position[1]-=1
+
+	def moveRight(self):
+		self.position[1]+=1
+
+	def __eq__(self,other):
+		self.type=other.type
+		self.shape=other.shape[:]
+		self.position=other.position[:]
---
+++ colors.py
@@ -0,0 +1,19 @@
+#================================================================#
+# Name        : colors.py                                        #
+# Description : Common color definitions                         #
+# Author      : Nguyen Hoang Nam                                 #
+# Date        : 07.11.2017                                       #
+#================================================================#
+
+WHITE      = (255,255,255)
+CYAN       = (68,255,255)
+YELLOW     = (255,255,68)
+ORANGE     = (255,136,0)
+GREEN      = (68,255,68)
+PINK       = (255,68,255)
+RED        = (240,0,0)
+BLUE1      = (0,0,240)
+BLUE       = (0,0,255)
+BLUE_DARK  = (0,0,150)
+BLACK      = (0,0,0)
+color={0:BLACK,1:PINK,2:RED,3:GREEN,4:YELLOW,5:CYAN,6:BLUE1,7:ORANGE}
